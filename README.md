447e51ff46e19b191c657fe324cd26444da208db
HoutSovannarith <hout.sovannarith2000@gmail.com>
2024-03-30 23:51:29 +0700

    Refactor robots.txt

--- README.md
+++ README.md
@@ -37,4 +37,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 
 ## My Projects website
 
-[My website](https://next-web-seo.netlify.app/)
+[My project in netlify](https://next-web-seo.netlify.app/)
+[My project in vercel](https://next-web-seo.vercel.app/)
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,20 @@
-'use client'
+"use client";
 import Image from "next/image";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
   return (
-      <div className="grid place-content-center h-screen">
-        <button onClick={() => router.push("/enroll")} className="p-4 bg-amber-700 rounded-xl">Enroll</button>
+    <div className="grid place-content-center h-screen">
+      <h2 className="uppercase text-4xl">Welcome to our store</h2>
+      <div className="grid place-content-center">
+        <button
+          onClick={() => router.push("/enroll")}
+          className="p-4 bg-amber-700 rounded-xl"
+        >
+          Enroll
+        </button>
       </div>
+    </div>
   );
 }
